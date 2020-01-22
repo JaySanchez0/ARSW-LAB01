a2426c5967a35c829eefd174feaf2caf39c75e22
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-01-22 13:19:33 -0500

    update

--- DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -4,10 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
+		//PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
 		
-		pft.start();
-		
+		//pft.start();
+		PrimeFinderThread p1=new PrimeFinderThread(0, 10000000);
+		p1.start();
+		PrimeFinderThread p2=new PrimeFinderThread(10000001, 20000000);
+		p2.start();
+		PrimeFinderThread p3=new PrimeFinderThread(20000001, 30000000);
+		p2.start();
 		
 	}
 	
